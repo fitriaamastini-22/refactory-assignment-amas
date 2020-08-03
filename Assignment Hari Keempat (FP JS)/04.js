1dc3986a40bd2edbef4c93fdb51332dfff2d1679
fitriaamastini <amastini22@gmail.com>
2020-08-04 04:46:48 +0700

    Perbaikan if Leap year

--- Assignment Hari Keempat (FP JS)/04.js
+++ Assignment Hari Keempat (FP JS)/04.js
@@ -3,7 +3,7 @@
 const leapYear = (year1, year2) =>{
 	let str = '';
 	for (let year = year1; year <= year2; year++){
-	    if( (year % 400 === 0) || (year % 4 === 0) ){
+	    if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
 	        str += `${year} is Leap Year\r\n`;
 	    }
 	    else{
